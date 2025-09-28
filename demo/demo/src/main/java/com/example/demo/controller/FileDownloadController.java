b888459c20dbe87645bcb780f20d01126cec6771
Namhehe237 <namnguyen23072003@gmail.com>
2025-09-28 20:11:59 +0700

    example of download file

---
+++ demo/demo/src/main/java/com/example/demo/controller/FileDownloadController.java
@@ -0,0 +1,50 @@
+package com.example.demo.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.UrlResource;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.net.MalformedURLException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+@RestController
+@RequestMapping("/api/files")
+public class FileDownloadController {
+
+    @Value("${app.file-storage.base-path}")
+    private String fileBasePath;
+
+    @GetMapping("/download/{fileName}")
+    public ResponseEntity<Resource> downloadFile(@PathVariable String fileName) {
+        try {
+            Path filePath = Paths.get(fileBasePath).resolve(fileName).normalize();
+            Resource resource = new UrlResource(filePath.toUri());
+            System.out.println(filePath.toString());
+            if (!resource.exists()) {
+                return ResponseEntity.notFound().build();
+            }
+
+            // Lấy content type
+            String contentType = Files.probeContentType(filePath);
+            if (contentType == null) {
+                contentType = "application/octet-stream";
+            }
+
+            return ResponseEntity.ok()
+                    .contentType(MediaType.parseMediaType(contentType))
+                    .header(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=\"" + resource.getFilename() + "\"")
+                    .body(resource);
+
+        } catch (MalformedURLException e) {
+            return ResponseEntity.badRequest().build();
+        } catch (Exception e) {
+            return ResponseEntity.internalServerError().build();
+        }
+    }
+}
